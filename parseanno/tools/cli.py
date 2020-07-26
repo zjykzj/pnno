1db8a6656717f3e47222b442a652b1451fc51f9d
zhujian <505169307@qq.com>
2020-07-26 10:47:44 +0800

    feat(argparse): 配置版本信息输出

--- parseanno/tools/cli.py
+++ parseanno/tools/cli.py
@@ -7,12 +7,21 @@
 @description: 
 """
 
+import sys
 from parseanno.config import cfg
 from parseanno.engine import AnnoProcessor, default_argument_parser
+from parseanno.utils.misc import get_version
 
 
 def main():
     args = default_argument_parser().parse_args()
+
+    if args.version:
+        print('ParseAnno: v{}'.format(get_version()))
+        sys.exit(0)
+
+    if not args.config_file:
+        raise ValueError('A configuration file path is required')
 
     cfg.merge_from_file(args.config_file)
     cfg.freeze()
---
+++ parseanno/utils/misc.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/7/26 上午10:36
+@file: misc.py
+@author: zj
+@description: 
+"""
+
+import parseanno
+
+
+def get_version():
+    return parseanno.__version__
