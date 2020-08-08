a6466680b8510fb3ab2318955fd40289e6e46423
zhujian <505169307@qq.com>
2020-08-08 16:13:29 +0800

    test(anno): labelimg_2_yolov5

---
+++ tests/labelimg_2_yolov5/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/8/8 下午3:48
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
---
+++ tests/labelimg_2_yolov5/test_labelimg_2_yolov5.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/8/8 下午3:48
+@file: test_labelimg_2_yolov5.py
+@author: zj
+@description: 
+"""
+
+from parseanno.config import cfg
+from parseanno.engine import AnnoProcessor
+
+
+class TestLabelImg2YoloV5():
+
+    def test(self):
+        config_file = 'configs/labelimg_2_yolov5.yaml'
+        cfg.merge_from_file(config_file)
+        cfg.freeze()
+
+        processor = AnnoProcessor(cfg)
+        processor.process()
