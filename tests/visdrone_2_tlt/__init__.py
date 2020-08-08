830343ba5aced771939bee8bfa686f15d0f63e8f
zhujian <505169307@qq.com>
2020-08-08 16:27:18 +0800

    test(anno): visdrone_2_tlt

---
+++ tests/visdrone_2_tlt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/8/8 下午4:15
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
---
+++ tests/visdrone_2_tlt/test_visdrone_2_tlt.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/8/8 下午4:16
+@file: test_visdrone_2_tlt.py
+@author: zj
+@description: 
+"""
+
+from parseanno.config import cfg
+from parseanno.engine import AnnoProcessor
+
+
+class TestVisdrone2Tlt():
+
+    def test(self):
+        config_file = 'configs/visdrone_2_tlt.yaml'
+        cfg.merge_from_file(config_file)
+        cfg.freeze()
+
+        processor = AnnoProcessor(cfg)
+        processor.process()
