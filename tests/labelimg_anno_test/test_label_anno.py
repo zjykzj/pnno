db768b68c6271ea3ae7136bf1a10dee7a3947580
zjkjzj <wy163zhuj@163.com>
2020-08-16 15:05:58 +0800

    test(labelimg_anno): 测试save功能

--- tests/labelimg_anno_test/test_label_anno.py
+++ tests/labelimg_anno_test/test_label_anno.py
@@ -7,8 +7,6 @@
 @description: 
 """
 
-import pytest
-
 from pnno.config import cfg
 from pnno.anno.labelimg_anno import LabelImgAnno
 
@@ -16,7 +14,7 @@ from pnno.anno.labelimg_anno import LabelImgAnno
 class TestLabelAnno(object):
 
     def test_process(self):
-        config_file = 'tests/labelimg_anno_test/config.yaml'
+        config_file = 'tests/labelimg_anno_test/process_config.yaml'
         cfg.merge_from_file(config_file)
         cfg.freeze()
 
@@ -24,10 +22,29 @@ class TestLabelAnno(object):
         output_data = labelImgAnno.process()
         print(output_data)
 
+        real_data = self.get_sample_data()
+
+        assert output_data == real_data
+
+    def test_save(self):
+        config_file = 'tests/labelimg_anno_test/save_config.yaml'
+        cfg.merge_from_file(config_file)
+        cfg.freeze()
+
+        labelImgAnno = LabelImgAnno(cfg)
+        output_data = self.get_sample_data()
+        output_data = labelImgAnno.save(output_data)
+
+    def get_sample_data(self):
+        """
+        {'classmap': {'box': 0},
+        'anno_data': {'tests/labelimg_anno_test/input_data/lena.jpg':
+        {'size': (512, 512), 'objects': [{'name': 'box', 'bndbox': [100, 100, 400, 400]}]}}}
+        """
         classmap = {'box': 0}
         anno_data = {
             'tests/labelimg_anno_test/input_data/lena.jpg': {
-                'size': (512, 512),
+                'size': (512, 512, 3),
                 'objects': [{'name': 'box', 'bndbox': [100, 100, 400, 400]}]
             }
         }
@@ -36,4 +53,4 @@ class TestLabelAnno(object):
             'anno_data': anno_data
         }
 
-        assert output_data == real_data
+        return real_data
